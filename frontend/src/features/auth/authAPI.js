da6a14b77ffad918f5d2d5d95e5ab5819f97548e
rohitkumar1801 <74654311+rohitkumar1801@users.noreply.github.com>
2023-07-05 10:01:27 +0530

    Update authAPI.js

--- frontend/src/features/auth/authAPI.js
+++ frontend/src/features/auth/authAPI.js
@@ -1,7 +1,6 @@
-
 export function createUser(userData) {
   return new Promise(async (resolve) => {
-    const response = await fetch('/auth/signup', {
+    const response = await fetch('https://go-kart-estore.onrender.com/auth/signup', {
       method: 'POST',
       body: JSON.stringify(userData),
       headers: { 'content-type': 'application/json' },
@@ -15,7 +14,7 @@ export function createUser(userData) {
 export function checkUser(loginInfo) {
   return new Promise(async (resolve, reject) => {
     try {
-      const response = await fetch('/auth/login', {
+      const response = await fetch('https://go-kart-estore.onrender.com/auth/login', {
         method: 'POST',
         body: JSON.stringify(loginInfo),
         headers: { 'content-type': 'application/json' },
