99ec2892630fb24e465fffcbb417218de0ab22dc
Rohit Kumar <rohitkumar.ff14@gmail.com>
2024-09-28 20:45:07 +0530

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.js";
 import "./index.css";
-import mainStore from "./store/Store.js";
+import mainStore from "./store/store.js";
 
 const root = createRoot(document.getElementById("root"));
 
