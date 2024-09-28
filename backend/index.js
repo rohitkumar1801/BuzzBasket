d15d2f5240789b89add129d5f13f4901f93346ed
Rohit Kumar <rohitkumar.ff14@gmail.com>
2024-09-28 21:30:15 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,6 @@ const ordersRouter = require("./routes/Order");
 const corsOptions = {
   origin: ["https://buzz-basket-front.vercel.app", "http://localhost:5173"], // Add frontend origins
   credentials: true, // Allow cookies and credentials
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Specify allowed methods
   allowedHeaders: ["Content-Type", "Authorization"], // Allow specific headers
   exposedHeaders: ["X-Total-Count"], // Expose custom headers like X-Total-Count
 };
@@ -32,25 +31,7 @@ app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname, "build")));
 
-app.use('/products', (req, res, next) => {
-    const allowedOrigins = ["https://buzz-basket-front.vercel.app", "http://localhost:5173"];
-    const origin = req.headers.origin;
-  
-    if (allowedOrigins.includes(origin)) {
-      res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-  
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  
-    // Handle preflight (OPTIONS) requests
-    if (req.method === 'OPTIONS') {
-      return res.sendStatus(200); // Preflight request response
-    }
-  
-    next();
-  });
+
 
 // to parse req.body
 app.use("/products", productsRouter);
