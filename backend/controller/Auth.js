11380551f98febe496783c2c00b91cc69cb319b4
Rohit Kumar <rohitkumar.ff14@gmail.com>
2025-02-23 17:13:09 +0530

    Update Auth.js

--- backend/controller/Auth.js
+++ backend/controller/Auth.js
@@ -36,6 +36,9 @@ exports.createUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true, // Prevents access to the cookie via JavaScript
       maxAge: 3600000, // 1 hour in milliseconds
+      secure: true, // must be true on HTTPS
+      sameSite: 'none' 
+
     });
 
     res.status(201).json({
